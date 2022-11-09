ec8c1890fcb99c6c1bc974322aeafe862320d5ad
ABIR <hbkabir004@gmail.com>
2022-11-10 01:22:43 +0600

    Services updated

--- src/components/Pages/AllServices.jsx
+++ src/components/Pages/AllServices.jsx
@@ -11,11 +11,6 @@ const AllServices = () => {
     }, [])
     return (
         <div>
-            <div className='text-center mt-20'>
-                <p className="inline-block px-5 py-2 text-sm font-semibold tracking-wider text-deep-purple-accent-400 uppercase rounded-full bg-gray-300">
-                    Services
-                </p>
-            </div>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                     <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
