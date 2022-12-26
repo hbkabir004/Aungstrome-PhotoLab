b60b8c67aa160773a193278e39999d81e4cee3c2
ABIR <hbkabir004@gmail.com>
2022-12-27 04:45:03 +0600

    Team Section Updated

--- src/components/Pages/Team.jsx
+++ src/components/Pages/Team.jsx
@@ -46,7 +46,7 @@ const Team = () => {
                     <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
                         <img
                             className="object-cover w-full h-56 md:h-64 xl:h-80"
-                            src="https://scontent.fjsr8-1.fna.fbcdn.net/v/t1.6435-9/50529011_2167455253569650_3596436576642531328_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGg4NBm-thEkgUyPfL0v_RSQQgFYkrENjxBCAViSsQ2PCA5CX2lIHQgdBB3LeoySZIicHf8rug-77iFmlkUJ6jU&_nc_ohc=kzI0H0Vo1PsAX8HJ-D7&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfCNPYRU-v4-4mT42pqdSpQag--2XLOv8RIsd9I_0ahLIg&oe=6392D61B"
+                            src="https://i.ibb.co/mbD0HK6/1.jpg"
                             alt="Person"
                         />
                         <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
@@ -59,16 +59,18 @@ const Team = () => {
                             </p>
                             <div className="flex items-center justify-center space-x-3">
                                 <a
-                                    href="/"
-                                    className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+                                    href="https://twitter.com/aungstrome"
+                                    target="_blank"
+                                    className="text-white transition-colors duration-300 hover:text-teal-accent-400" rel="noreferrer"
                                 >
                                     <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                                         <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
                                     </svg>
                                 </a>
                                 <a
-                                    href="/"
-                                    className="text-white transition-colors duration-300 hover:text-teal-accent-400"
+                                    href="https://www.facebook.com/abircse.nwu/"
+                                    target="_blank"
+                                    className="text-white transition-colors duration-300 hover:text-teal-accent-400" rel="noreferrer"
                                 >
                                     <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                                         <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
@@ -82,7 +84,7 @@ const Team = () => {
                     <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
                         <img
                             className="object-cover w-full h-56 md:h-64 xl:h-80"
-                            src="https://scontent.fjsr8-1.fna.fbcdn.net/v/t39.30808-6/314562385_5381577858607607_8154966350035518981_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHLnoGfQSQOw1JTcQddFGCe37ot-6t9vNzfui37q3283E9yhyWJ5obigxbEdVkdasBPE4VCeWZDVn5WARCBNu-4&_nc_ohc=iTbDSENGobgAX9psyFz&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfCAnMKsWq_EUwp770UL_bpXyrD4TlFiOwGD34bknJNoFg&oe=636F8BF3"
+                            src="https://i.ibb.co/VMQYZZD/4.jpg"
                             alt="Person"
                         />
                         <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
@@ -118,7 +120,7 @@ const Team = () => {
                     <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
                         <img
                             className="object-cover w-full h-56 md:h-64 xl:h-80"
-                            src="https://scontent.fjsr8-1.fna.fbcdn.net/v/t1.6435-9/72185653_1013689458967691_1395892030789386240_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHI4pN1wPKqxFy7pHxe2ELO7UpbsOK4qdDtSluw4rip0Gahqnh1U-rubSgk1g08yXYWF-ZRhN9PDI0RtWgyBHiq&_nc_ohc=GoGJbhj3LFAAX-Hztmb&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfATR4yQV25LDPEaBnqxZvoJwxl3sId8BDxJYNk-uNJitA&oe=6392DA0D"
+                            src="https://i.ibb.co/KyhzQ8k/2.jpg"
                             alt="Person"
                         />
                         <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
@@ -154,7 +156,7 @@ const Team = () => {
                     <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
                         <img
                             className="object-cover w-full h-56 md:h-64 xl:h-80"
-                            src="https://scontent.fjsr8-1.fna.fbcdn.net/v/t31.18172-8/22095897_740571849461290_2437389895257699801_o.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFCE-VD26PCDJEATqI9sVw4M0KIKOx6818zQogo7HrzXxEB1nxvj66zBQu7W8anqEPMnLMuPVXDtYQpvNKsuFxh&_nc_ohc=Qi6XMl9SWnEAX9EiyuC&_nc_ht=scontent.fjsr8-1.fna&oh=00_AfDK3jWwLsU4o3nHHUtVawYZdLLh3CATvYoCXzY6JCPbFA&oe=6392BA16"
+                            src="https://i.ibb.co/4gtppvF/3.jpg"
                             alt="Person"
                         />
                         <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
